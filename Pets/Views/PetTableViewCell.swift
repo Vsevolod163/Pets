b4b193de02ef7ec9db74318c7de8c13cdd98d979
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-25 15:16:30 +0300

    PetTableViewCell class added

---
+++ Pets/Views/PetTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  PetTableViewCell.swift
+//  Pets
+//
+//  Created by Vsevolod Lashin on 25.05.2023.
+//
+
+import UIKit
+import Kingfisher
+
+final class PetTableViewCell: UITableViewCell {
+
+    @IBOutlet var petImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    
+    func configure(with pet: Animal) {
+        let imageURL = URL(string: pet.photo)
+        
+        nameLabel.text = pet.name
+        petImageView.kf.setImage(with: imageURL)
+    }
+}
