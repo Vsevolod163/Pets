904928d0f4df00e69d722d206cfdcb2aca3834dc
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-27 22:40:36 +0300

    write and save functions added

--- Pets/Services/StorageManager.swift
+++ Pets/Services/StorageManager.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import RealmSwift
+
+final class StorageManager {
+    static var shared = StorageManager()
+    let realm: Realm!
+
+    private init() {
+        do {
+            realm = try Realm()
+        } catch {
+            fatalError("Failed to initialize Realm: \(error)")
+        }
+    }
+    
+    private func save(_ animals: [Animal]) {
+        write {
+            realm.add(animals)
+        }
+    }
+    
+    private func write(completion: () -> Void) {
+        do {
+            try realm.write {
+                completion()
+            }
+        } catch {
+            print(error)
+        }
+    }
+}
