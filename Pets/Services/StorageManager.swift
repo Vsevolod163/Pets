216ce3717539df24aa18d7282a8e3e6174a9a9dd
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-30 14:42:43 +0300

    StorageManager added

--- Pets/Services/StorageManager.swift
+++ Pets/Services/StorageManager.swift
@@ -6,7 +6,66 @@
 //
 
 import Foundation
+import CoreData
 
 final class StorageManager {
- 
+    static var shared = StorageManager()
+    
+    // MARK: - Core Data stack
+    private let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "TaskList")
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        
+        return container
+    }()
+    
+    private let viewContext: NSManagedObjectContext
+    
+    private init() {
+        viewContext = persistentContainer.viewContext
+    }
+    
+    // MARK: - CRUD
+    func create(_ animalName: String, completion: (CurrentAnimal) -> Void) {
+        let animal = CurrentAnimal(context: viewContext)
+        animal.name = animalName
+        completion(animal)
+        saveContext()
+    }
+    
+    func fetchData(completion: (Result<[CurrentAnimal], Error>) -> Void) {
+        let fetchRequest = CurrentAnimal.fetchRequest()
+        
+        do {
+            let animals = try viewContext.fetch(fetchRequest)
+            completion(.success(animals))
+        } catch let error {
+            completion(.failure(error))
+        }
+    }
+    
+    func update(_ animal: CurrentAnimal, newName: String) {
+        animal.name = newName
+        saveContext()
+    }
+    
+    func delete(_ animal: CurrentAnimal) {
+        viewContext.delete(animal)
+        saveContext()
+    }
+    
+    func saveContext() {
+        if viewContext.hasChanges {
+            do {
+                try viewContext.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
 }
